2f3382c8c338990b48e96e1a3b58717dcdf37bd2
shenyaoqiang <shenyaoqiang45@163.com>
2020-01-13 19:15:38 +0800

    delete useless

--- test_struct/include/fib.h
+++
@@ -1,6 +0,0 @@
-#ifndef _TEST_FIB_
-#define _TEST_FIB_
-
-int fib(int n);
-
-#endif 
--- test_struct/src/fib.cpp
+++
@@ -1,8 +0,0 @@
-#include "fib.h"
-
-int fib(int n)
-{
-    if(0 == n || 1 == n)
-        return 1;
-    return fib(n-1)+fib(n-2);
-}
